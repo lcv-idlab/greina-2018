6d20027a41a4515bad47c53741ae0027605964b5
marcluralab <marco@marcolurati.ch>
2018-04-25 08:50:30 +0200

    model greina interface encoder code arduino

--- Greina_model_arduino/Greina_model_arduino.ino
+++ Greina_model_arduino/Greina_model_arduino.ino
@@ -7,52 +7,38 @@
   Marco Lurati
 */
 
-const int pinNext = 2;
-const int pinPrev = 3;
+#define outputA 6
+#define outputB 7
+
+int aState;
+int aLastState; 
 
 boolean next, prev, old_next, old_prev;
-
-const int antibump = 100;
-
-long old_millis_next, old_millis_prev;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(pinNext, INPUT);
-  pinMode(pinPrev, INPUT);
+  pinMode (outputA,INPUT);
+  pinMode (outputB,INPUT);
 
+  // Reads the initial state of the outputA
+   aLastState = digitalRead(outputA);   
 }
 
 void loop() {
 
-  next = digitalRead(pinNext);
-  prev = digitalRead(pinPrev);
-
-  if(next) {
-    if(!old_next) {
-      if(millis() > old_millis_next + antibump) {
-        Serial.println(1);
-        old_next = true;
-        old_millis_next = millis();
-        Serial.println(0);
-      }
+  aState = digitalRead(outputA); // Reads the "current" state of the outputA
+  // If the previous and the current state of the outputA are different, that means a Pulse has occured
+  if (aState != aLastState){     
+  // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+    if (digitalRead(outputB) != aState) { 
+      Serial.println(1);
+      Serial.println(0);
+    } else {
+      Serial.println(2);
+      Serial.println(0);
     }
-  } else {
-    old_next = false;
-  }
-
-  if(prev) {
-    if(!old_prev) {
-      if(millis() > old_millis_prev + antibump) {
-        Serial.println(2);
-        old_prev = true;
-        old_millis_prev = millis();
-        Serial.println(0);
-      }
-    }
-  } else {
-    old_prev = false;
-  }
+  } 
+  aLastState = aState; // Updates the previous state of the outputA with the current state
   
 }
